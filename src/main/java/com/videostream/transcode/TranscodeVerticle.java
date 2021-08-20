82af7bd18536d45c110eb624e9334ea03e19f25e
Minhaj Md Rubaiat Alam <mrl@vizrt.com>
2021-08-20 18:55:47 +0200

    Reformat code

--- src/main/java/com/videostream/transcode/TranscodeVerticle.java
+++ src/main/java/com/videostream/transcode/TranscodeVerticle.java
@@ -13,73 +13,70 @@ import net.bramp.ffmpeg.builder.FFmpegBuilder;
 
 import java.io.IOException;
 
-public class TranscodeVerticle  extends AbstractVerticle {
-  String requestJson = null;
-  private String mediaDirectory;
+public class TranscodeVerticle extends AbstractVerticle {
+    String requestJson = null;
+    private String mediaDirectory;
 
-  public String getRequestJson() {
-    return requestJson;
-  }
+    public String getRequestJson() {
+        return requestJson;
+    }
 
-  public void setRequestJson(String requestJson) {
-    this.requestJson = requestJson;
-  }
+    public void setRequestJson(String requestJson) {
+        this.requestJson = requestJson;
+    }
 
-  public String getMediaDirectory() {
-    return mediaDirectory;
-  }
+    public String getMediaDirectory() {
+        return mediaDirectory;
+    }
 
-  public void setMediaDirectory(String mediaDirectory) {
-    this.mediaDirectory = mediaDirectory;
-  }
+    public void setMediaDirectory(String mediaDirectory) {
+        this.mediaDirectory = mediaDirectory;
+    }
 
-  @Override
-  public void start(Promise<Void> startPromise) throws Exception {
-    VertxOptions options = new VertxOptions();
-    options.setBlockedThreadCheckInterval(300L*1000*1000*1000);
-    Vertx.vertx(options).executeBlocking(promise -> {
-
-//    });
-//
-//    vertx.executeBlocking(promise -> {
-      ObjectMapper obj = new ObjectMapper();
-      TranscodeDTO transcodeJob = null;
-      try {
-        transcodeJob = obj.readValue(this.requestJson, TranscodeDTO.class);
-      } catch (JsonProcessingException e) {
-        e.printStackTrace();
-      }
-      System.out.println(transcodeJob.getId());
-      FFmpeg ffmpeg = null;
-      FFprobe ffprobe = null;
-      try {
-        ffmpeg = new FFmpeg("/usr/bin/ffmpeg");
-        ffprobe = new FFprobe("/usr/bin/ffprobe");
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-      FFmpegBuilder builder = new FFmpegBuilder()
-        .setInput(mediaDirectory + transcodeJob.getFileId() + "/" + transcodeJob.getFileName())     // Filename, or a FFmpegProbeResult
-        .overrideOutputFiles(true) // Override the output if it exists
-        .addOutput(mediaDirectory + transcodeJob.getFileId() + "/" + transcodeJob.getOutputFileName())   // Filename for the destination
-        .setFormat(transcodeJob.getTargetFormat())        // Format is inferred from filename, or can be set
+    @Override
+    public void start(Promise<Void> startPromise) throws Exception {
+        VertxOptions options = new VertxOptions();
+        options.setBlockedThreadCheckInterval(300L * 1000 * 1000 * 1000);
+        Vertx.vertx(options).executeBlocking(promise -> {
+            ObjectMapper obj = new ObjectMapper();
+            TranscodeDTO transcodeJob = null;
+            try {
+                transcodeJob = obj.readValue(this.requestJson, TranscodeDTO.class);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+            System.out.println(transcodeJob.getId());
+            FFmpeg ffmpeg = null;
+            FFprobe ffprobe = null;
+            try {
+                ffmpeg = new FFmpeg("/usr/bin/ffmpeg");
+                ffprobe = new FFprobe("/usr/bin/ffprobe");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            FFmpegBuilder builder = new FFmpegBuilder()
+                .setInput(mediaDirectory + transcodeJob.getFileId() + "/" + transcodeJob.getFileName())     // Filename, or a FFmpegProbeResult
+                .overrideOutputFiles(true) // Override the output if it exists
+                .addOutput(mediaDirectory + transcodeJob.getFileId() + "/" + transcodeJob.getOutputFileName())   // Filename for the destination
+                .setFormat(transcodeJob.getTargetFormat())        // Format is inferred from filename, or can be set
 //                .setTargetSize(250_000)  // Aim for a 250KB file
-        .disableSubtitle()       // No subtiles
-        .setAudioChannels(1)         // Mono audio
-        .setAudioCodec("aac")        // using the aac codec
-        .setAudioSampleRate(48_000)  // at 48KHz
-        .setAudioBitRate(32768)      // at 32 kbit/s
-        .setVideoCodec("libx264")     // Video using x264
-        .setVideoFrameRate(24, 1)     // at 24 frames per second
-        .setVideoResolution(640, 480) // at 640x480 resolution
-        .setStrict(FFmpegBuilder.Strict.EXPERIMENTAL) // Allow FFmpeg to use experimental specs
-        .done();
-      FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
-      executor.createJob(builder).run();
-      promise.complete();
-    }).onComplete(result -> {
-      System.out.println("Transcode job completed");
-    });
-
-  }
+                .disableSubtitle()       // No subtiles
+                .setAudioChannels(1)         // Mono audio
+                .setAudioCodec("aac")        // using the aac codec
+                .setAudioSampleRate(48_000)  // at 48KHz
+                .setAudioBitRate(32768)      // at 32 kbit/s
+                .setVideoCodec("libx264")     // Video using x264
+                .setVideoFrameRate(24, 1)     // at 24 frames per second
+                .setVideoResolution(640, 480) // at 640x480 resolution
+                .setStrict(FFmpegBuilder.Strict.EXPERIMENTAL) // Allow FFmpeg to use experimental specs
+                .done();
+            FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+            executor.createJob(builder).run();
+            // Or run a two-pass encode (which is better quality at the cost of being slower)
+            // executor.createTwoPassJob(builder).run();
+            promise.complete();
+        }).onComplete(result -> {
+            System.out.println("Transcode job completed");
+        });
+    }
 }
