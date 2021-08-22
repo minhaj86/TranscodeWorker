540f44f260e81518fb0bd433a300388906d55961
Minhaj Md Rubaiat Alam <mrl@vizrt.com>
2021-08-22 03:13:25 +0200

    HTTP Method and Resouce Path check for File uploader API

--- src/main/java/com/videostream/transcode/UploadServiceVerticle.java
+++ src/main/java/com/videostream/transcode/UploadServiceVerticle.java
@@ -1,5 +1,6 @@
 package com.videostream.transcode;
 
+import io.vertx.codegen.annotations.Nullable;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.file.AsyncFile;
 import io.vertx.core.file.OpenOptions;
@@ -13,19 +14,37 @@ public class UploadServiceVerticle extends AbstractVerticle {
     public void start() throws Exception {
         vertx.createHttpServer().requestHandler(req -> {
             String method = req.method().name();
+            @Nullable String resourcePath = req.path();
             System.out.println("HTTP Method: " + method);
+            System.out.println("HTTP Path: " + resourcePath);
             req.pause();
-            String filename = UUID.randomUUID() + ".uploaded";
-            vertx.fileSystem().open(filename, new OpenOptions(), ares -> {
-                AsyncFile file = ares.result();
-                Pump pump = Pump.pump(req, file);
-                req.endHandler(v1 -> file.close(v2 -> {
-                    System.out.println("Uploaded to " + filename);
+            if (method.equals("POST") && resourcePath.equals("/upload")) {
+                String filename = UUID.randomUUID() + ".uploaded";
+                vertx.fileSystem().open(filename, new OpenOptions(), ares -> {
+                    AsyncFile file = ares.result();
+                    Pump pump = Pump.pump(req, file);
+                    req.endHandler(v1 -> file.close(v2 -> {
+                        System.out.println("Uploaded to " + filename);
+                        req.response().end();
+                    }));
+                    pump.start();
+                    req.resume();
+                });
+            } else if (!resourcePath.equals("/upload")) {
+                System.out.println("Wrong path");
+                req.endHandler(v1 -> {
+                    req.response().setStatusCode(404);
                     req.response().end();
-                }));
-                pump.start();
+                });
                 req.resume();
-            });
+            } else if (!method.equals("POST")) {
+                System.out.println("Wrong method");
+                req.endHandler(v1 -> {
+                    req.response().setStatusCode(405);
+                    req.response().end();
+                });
+                req.resume();
+            }
         }).listen(8080);
     }
 }
