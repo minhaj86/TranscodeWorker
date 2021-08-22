b5525f857f80f0ee4530195e32f02ec66443e6ea
Minhaj Md Rubaiat Alam <mrl@vizrt.com>
2021-08-23 00:31:08 +0200

    Better handling of HTTP request with Vert.x-web Router class

--- src/main/java/com/videostream/transcode/UploadServiceVerticle.java
+++ src/main/java/com/videostream/transcode/UploadServiceVerticle.java
@@ -4,47 +4,36 @@ import io.vertx.codegen.annotations.Nullable;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.file.AsyncFile;
 import io.vertx.core.file.OpenOptions;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.streams.Pump;
+import io.vertx.ext.web.Router;
 
 import java.util.UUID;
 
 public class UploadServiceVerticle extends AbstractVerticle {
-
+    private String MEDIA_DIRECTORY = null;
     @Override
     public void start() throws Exception {
-        vertx.createHttpServer().requestHandler(req -> {
-            String method = req.method().name();
-            @Nullable String resourcePath = req.path();
-            System.out.println("HTTP Method: " + method);
-            System.out.println("HTTP Path: " + resourcePath);
-            req.pause();
-            if (method.equals("POST") && resourcePath.equals("/upload")) {
-                String filename = UUID.randomUUID() + ".uploaded";
-                vertx.fileSystem().open(filename, new OpenOptions(), ares -> {
-                    AsyncFile file = ares.result();
-                    Pump pump = Pump.pump(req, file);
-                    req.endHandler(v1 -> file.close(v2 -> {
-                        System.out.println("Uploaded to " + filename);
-                        req.response().end();
-                    }));
-                    pump.start();
-                    req.resume();
-                });
-            } else if (!resourcePath.equals("/upload")) {
-                System.out.println("Wrong path");
-                req.endHandler(v1 -> {
-                    req.response().setStatusCode(404);
-                    req.response().end();
-                });
-                req.resume();
-            } else if (!method.equals("POST")) {
-                System.out.println("Wrong method");
-                req.endHandler(v1 -> {
-                    req.response().setStatusCode(405);
-                    req.response().end();
-                });
-                req.resume();
-            }
-        }).listen(8080);
+        this.MEDIA_DIRECTORY = System.getenv("mediadirectory");
+        Router router = Router.router(vertx);
+        router.route(HttpMethod.POST, "/api/upload/:videoId/:fileName").handler(routingContext-> {
+            HttpServerRequest httpRequest = routingContext.request();
+            httpRequest.pause();
+            @Nullable String videoId = httpRequest.getParam("videoId");
+            @Nullable String fileName = httpRequest.getParam("fileName");
+            String filePath = this.MEDIA_DIRECTORY + videoId + "/" + UUID.randomUUID() + "_" + fileName;
+            vertx.fileSystem().open(filePath, new OpenOptions(), ares -> {
+                AsyncFile file = ares.result();
+                Pump pump = Pump.pump(httpRequest, file);
+                httpRequest.endHandler(v1 -> file.close(v2 -> {
+                    System.out.println("Uploaded to " + filePath);
+                    httpRequest.response().end();
+                }));
+                pump.start();
+                httpRequest.resume();
+            });
+        });
+        vertx.createHttpServer().requestHandler(router).listen(8080);
     }
 }
